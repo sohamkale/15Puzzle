8cf9991d416d3c21fe224c5be991aeda8c2c14b8
Kelvin <doverkd1@gmail.com>
2020-06-26 22:13:49 -0400

    Attempted to make number material change dynamically- Not finished

--- 15Puzzle/Assets/Scripts/UpdateTileSprite.cs
+++ 15Puzzle/Assets/Scripts/UpdateTileSprite.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class UpdateTileSprite : MonoBehaviour
 {
-
+    public Material v_MaterialToLoad;
+    public GameObject v_thisObject;
+    public bool v_doOnce = false;
     // Start is called before the first frame update
     void Start()
     {
- 
-    }
+      }
 
     // Update is called once per frame
     void Update()
     {
-     
+        if (GlobalVariables.v_solvableboard && !v_doOnce)
+        {
+            int temp = gameObject.GetComponent<Tile>().v_TileID;
+           v_MaterialToLoad =  Resources.Load("Numbers/Materials/"+temp.ToString(), typeof(Material)) as Material;
+           
+            v_doOnce = true;
+            Renderer rend = GetComponent<Renderer>();
+            if (rend != null)
+            {
+                rend.material = v_MaterialToLoad;
+            }
+             
+        }
     }
 }
